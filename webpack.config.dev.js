032dff9cb364dad23764c42ede16c03a2259e6db
Leon <yanglyu.leon.7@gmail.com>
2019-11-11 14:46:11 -0800

    fix: Correcting the config setup

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,7 +7,7 @@ process.env.NODE_ENV = "development";
 module.exports = {
   mode: "development",
   target: "web",
-  devtool: "cheap-module-source-map",
+  devTool: "cheap-module-source-map",
   entry: "./src/index",
   output: {
     path: path.resolve(__dirname, "build"),
@@ -31,7 +31,7 @@ module.exports = {
   modules: {
     rules: [
       {
-        test: /\.(js | jsx)$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ["babel-loader", "eslint-loader"]
       },
