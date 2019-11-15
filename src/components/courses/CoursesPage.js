3489929e0864cad1782412b1a2fda0f8e5fd5597
Leon <yanglu@microsoft.com>
2019-11-14 17:31:51 -0800

    tutorial: Mapping manually, mapDispatchToProps
    module: 9.5

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -17,7 +17,7 @@ class CoursesPage extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault()
-    this.props.dispatch(courseActions.createCourse(this.state.course))
+    this.props.createCourse(this.state.course)
   }
 
   render () {
@@ -31,13 +31,17 @@ class CoursesPage extends React.Component {
           value={this.state.course.title}
         />
         <input type='submit' value='Save' />
+        {this.props.courses.map(course => (
+          <div key={course.title}>{course.title}</div> // Keys help React track each array element
+        ))}
       </form>
     )
   }
 }
 
 CoursesPage.propTypes = {
-  dispatch: PropTypes.func.isRequired
+  courses: PropTypes.array.isRequired,
+  createCourse: PropTypes.func.isRequired
 }
 
 function mapStateToProps (state) {
@@ -46,8 +50,24 @@ function mapStateToProps (state) {
   }
 }
 
+/**
+ * This determines what actions are available on props in our component
+ * @param {} dispatch
+ */
+function mapDispathToProps (dispatch) {
+  return {
+    // Remember: if one doesn't call dispatch, nothing will happen.
+    // Action creators must be called by dispatch.
+    // Dispatch[function] is the function to notify React about an Action
+    createCourse: course => dispatch(courseActions.createCourse(course))
+  }
+}
+
 /*
  * When mapDispathToProps is omitted,
  * our component gets a dispatch prop injected automatically
  */
-export default connect(mapStateToProps)(CoursesPage)
+export default connect(
+  mapStateToProps,
+  mapDispathToProps
+)(CoursesPage)
