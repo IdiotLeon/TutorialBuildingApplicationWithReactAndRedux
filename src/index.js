4b7aafdb9f877c0b166456be4bf3ee48340cce8a
Leon <yanglu@microsoft.com>
2019-11-14 16:23:12 -0800

    tutorial: instantiating the store and provider
    module: 9.7

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from "react";
-import { render } from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./components/App";
-import "./index.css";
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './components/App'
+import './index.css'
+import configreStore from './redux/configureStore'
+import { Provider as ReduxProvider } from 'react-redux'
+
+/*
+ * It can be useful to pass initial state into the store here
+ * if you're server rendering or intializing your Redux store
+ * with data from localStorage
+ */
+const store = configreStore()
 
 render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById("app")
-);
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
+  document.getElementById('app')
+)
